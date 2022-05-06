7787cc93ad89708e4a17bc941f9f500479cbb6b4
BruguierJeremy <jeremybruguier0@gmail.com>
2022-05-06 16:07:12 +0200

    Mise en place du dispatch

--- public/index.php
+++ public/index.php
@@ -44,3 +44,20 @@ $router->map(
     'main-home'
 );
 
+/* -------------
+--- DISPATCH ---
+--------------*/
+
+// On demande à AltoRouter de trouver une route qui correspond à l'URL courante
+$match = $router->match();
+
+
+// Ensuite, pour dispatcher le code dans la bonne méthode, du bon Controller
+// On délègue à une librairie externe : https://packagist.org/packages/benoclock/alto-dispatcher
+// 1er argument : la variable $match retournée par AltoRouter
+// 2e argument : le "target" (controller & méthode) pour afficher la page 404
+$dispatcher = new Dispatcher($match, '\App\Controllers\ErrorController::err404');
+
+$dispatcher->setControllersArguments($router, $match);
+// Une fois le "dispatcher" configuré, on lance le dispatch qui va exécuter la méthode du controller
+$dispatcher->dispatch();
