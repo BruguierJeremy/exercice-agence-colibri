75c1070b3da372c1ccf7f2ac53b04c2ce270f930
BruguierJeremy <jeremybruguier0@gmail.com>
2022-05-06 16:06:24 +0200

    Mise en place du routeur

---
+++ public/index.php
@@ -0,0 +1,46 @@
+<?php
+
+//permet de charger d'un coup toutes les dépendances installées avec composer
+require_once '../vendor/autoload.php';
+
+//on démarre la session
+session_start();
+
+/* ------------
+--- ROUTAGE ---
+-------------*/
+
+// création de l'objet router
+$router = new AltoRouter();
+
+// Si il y a un sous-répertoire
+if (array_key_exists('BASE_URI', $_SERVER)) {
+    // Alors on définit le basePath d'AltoRouter
+    $router->setBasePath($_SERVER['BASE_URI']);
+    // ainsi, nos routes correspondront à l'URL, après la suite de sous-répertoire
+} else { // sinon
+    // On donne une valeur par défaut à $_SERVER['BASE_URI']
+    $_SERVER['BASE_URI'] = '/';
+}
+
+// On doit déclarer toutes les "routes" à AltoRouter,
+// afin qu'il puisse nous donner LA "route" correspondante à l'URL courante
+// 1. méthode HTTP
+// 2. La route : la portion d'URL après le basePath
+// 3. Target/Cible : informations contenant
+//      - le nom de la méthode à utiliser pour répondre à cette route
+//      - le nom du controller contenant la méthode
+// 4. Le nom de la route : pour identifier la route, on va suivre une convention
+//      - "NomDuController-NomDeLaMéthode"
+//      - ainsi pour la route /, méthode "home" du MainController => "main-home"
+$router->map(
+    'GET',
+    '/',
+    [
+        'method' => 'home',
+        'controller' => '\App\Controllers\MainController',
+        'role' => ['admin', 'catalog-manager'],
+    ],
+    'main-home'
+);
+
