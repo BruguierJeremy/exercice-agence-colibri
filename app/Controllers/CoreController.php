0b5a171b4951e69f72f894f8098fa91ff028d558
BruguierJeremy <jeremybruguier0@gmail.com>
2022-05-06 16:19:40 +0200

    Création du CoreController

---
+++ app/Controllers/CoreController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+class CoreController 
+{
+
+    /**
+     * Méthode permettant d'afficher du code HTML en se basant sur les views
+     *
+     * @param string $viewName Nom du fichier de vue
+     * @param array $viewData Tableau des données à transmettre aux vues
+     * @return void
+     */
+    protected function show( string $viewName, $viewData = [])
+    {
+        // Comme $viewData est déclarée comme paramètre de la méthode show()
+        // les vues y ont accès
+        // ici une valeur dont on a besoin sur TOUTES les vues
+        // donc on la définit dans show()
+        $viewData['currentPage'] = $viewName;
+
+        // définir l'url absolue pour nos assets
+        $viewData['assetsBaseUri'] = $_SERVER['BASE_URI'] . 'assets/';
+
+        // définir l'url absolue pour la racine du site
+        $viewData['baseUri'] = $_SERVER['BASE_URI'];
+
+        // La fonction extract permet de créer une variable pour chaque élément du tableau passé en argument
+        extract($viewData);
+
+        // $viewData est disponible dans chaque fichier de vue
+        require_once __DIR__ . '/../views/layout/header.tpl.php';
+        require_once __DIR__ . '/../views/' . $viewName . '.tpl.php';
+        require_once __DIR__ . '/../views/layout/footer.tpl.php';
+    }
+
+}
